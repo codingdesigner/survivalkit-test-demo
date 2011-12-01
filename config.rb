e6910a92f5013e7d792682a73228378f75fb87f5
Mason Wendell <mason@zivtech.com>
2011-12-01 01:48:47 -0500

    compiled css and config

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 require 'survivalkit'
+require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -12,10 +13,10 @@ javascripts_dir = "js"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
